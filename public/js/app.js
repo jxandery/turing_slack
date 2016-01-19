9bd6052e01ea818113b53fdcedf6d14f2b31c90a
Jack Yeh <jack.yeh@gmail.com>
2016-01-18 22:01:35 -0700

    add functionality: message form field is reset after clicking on submit

--- public/js/app.js
+++ public/js/app.js
@@ -15,8 +15,10 @@ $form.on('submit', function(event) {
   event.preventDefault();
 
   var $message = $form.find('input[name=message]');
-
   socket.emit('message', {
     text: $message.val()
   });
+
+  $message.val('');
+
 });
