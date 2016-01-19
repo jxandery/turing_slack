901f79f2941d7723797abc5cad933c46034e9a87
Jack Yeh <jack.yeh@gmail.com>
2016-01-18 20:13:43 -0700

    setup: connect server with socket

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var io        = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
+io.on('connection', function(socket) {
+  console.log('User connected via socket.io!');
+});
+
 http.listen(PORT, function() {
   console.log('Server started!');
 });
